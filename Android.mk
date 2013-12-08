782985ed57030f901183d2f77267862920b53a4d
pelya <x.pelya.x@gmail.com>
2013-12-08 15:53:05 +0200

    Fixes to Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := andorid-shmem
+LOCAL_MODULE := android-shmem
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_CFLAGS := -O3 -I$(LOCAL_PATH)/libancillary
@@ -15,6 +15,6 @@ LOCAL_SHARED_LIBRARIES := -llog
 
 LOCAL_STATIC_LIBRARIES := 
 
-LOCAL_LDLIBS := 
+LOCAL_LDFLAGS := -Wl,--version-script=$(LOCAL_PATH)/exports.txt
 
 include $(BUILD_SHARED_LIBRARY)
